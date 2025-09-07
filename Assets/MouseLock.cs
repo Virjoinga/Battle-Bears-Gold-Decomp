b0ccd309fbf84a79b82e251d9754b5cbb7de669c
Virjoinga <ethanbest51@gmail.com>
2025-09-07 12:07:26 +1200

    Introduce mouse locking solution
    - Automatically confines and/or locks the cursor.

---
+++ Assets/MouseLock.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MouseLock : MonoBehaviour 
+{
+    private PlayerController playerController;
+
+    void Awake()
+	{
+        if (Application.platform != RuntimePlatform.Android || Application.platform != RuntimePlatform.IPhonePlayer)
+        {
+            DontDestroyOnLoad(this.gameObject);
+        } 
+    }
+
+    void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "MainMenu")
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+
+        GameObject pauseMenu = GameObject.Find("pause(Clone)");
+        GameObject statsOverlay = GameObject.Find("teamStats_overlay(Clone)");
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");       
+        if (player != null )
+        {
+            playerController = player.GetComponent<PlayerController>();
+            if (playerController != null )
+            {
+                if (!playerController.IsDead && pauseMenu == null && statsOverlay == null && !CustomLayoutController.Instance.IsOpen)
+                {
+                    LockMouse();
+                }
+                else
+                {
+                    UnlockMouse();
+                }
+            }
+        }
+    }
+    private void LockMouse()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void UnlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+}
