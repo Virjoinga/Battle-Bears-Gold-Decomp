639c2b62774d50ae028ad7c74750a3808f1ceb28
Joe Rice <joerice123455t@gmail.com>
2025-09-19 01:46:56 +0100

    Revert "WILL REVERT IF NOT WORKING"
    This reverts commit b2435fc6960a8ab325e1f10004cd28e7d2439b0a.

--- Assets/Scripts/Assembly-CSharp/Projectile.cs
+++ Assets/Scripts/Assembly-CSharp/Projectile.cs
@@ -1,148 +1,149 @@
 using UnityEngine;
+
 public class Projectile : ConfigurableNetworkObject
 {
-    public ConfigurableNetworkObject objectToSpawn;
+	public ConfigurableNetworkObject objectToSpawn;
 
-    protected bool hasSpawned;
+	protected bool hasSpawned;
 
-    public string spawnItemOverride = string.Empty;
+	public string spawnItemOverride = string.Empty;
 
-    private TeslaShield tesla;
+	private TeslaShield tesla;
 
-    public bool ignoreOwner;
+	public bool ignoreOwner;
 
-    public bool explodeOnCollision = true;
+	public bool explodeOnCollision = true;
 
-    public bool explodeOnTriggerEnter = true;
+	public bool explodeOnTriggerEnter = true;
 
-    [SerializeField]
-    protected bool _tryToSpawnOnDisable;
+	[SerializeField]
+	protected bool _tryToSpawnOnDisable;
 
-    [SerializeField]
-    protected bool _orientX;
+	[SerializeField]
+	protected bool _orientX;
 
-    [SerializeField]
-    protected bool _orientY;
+	[SerializeField]
+	protected bool _orientY;
 
-    [SerializeField]
-    protected bool _orientZ;
+	[SerializeField]
+	protected bool _orientZ;
 
-    [SerializeField]
-    protected bool shakeScreen;
+	[SerializeField]
+	protected bool shakeScreen;
 
-    [SerializeField]
-    protected float shakeDuration;
+	[SerializeField]
+	protected float shakeDuration;
 
-    [SerializeField]
-    protected float shakeStrength;
+	[SerializeField]
+	protected float shakeStrength;
 
-    [SerializeField]
-    protected float explosionShakeRadius;
+	[SerializeField]
+	protected float explosionShakeRadius;
 
-    [SerializeField]
-    protected float shakeFunctionExponent = 3f;
+	[SerializeField]
+	protected float shakeFunctionExponent = 3f;
 
-    protected string EquipmentNames
-    {
-        get
-        {
-            return equipmentNames;
-        }
-    }
+	protected string EquipmentNames
+	{
+		get
+		{
+			return equipmentNames;
+		}
+	}
 
-    protected virtual void OnTriggerEnter(Collider c)
-    {
-        tesla = c.GetComponent<TeslaShield>();
-        if ((!(tesla != null) || !tesla.PlayerOnOwnersTeam(base.OwnerID)) && explodeOnTriggerEnter)
-        {
-            Explode(c.gameObject);
-        }
-    }
+	protected virtual void OnTriggerEnter(Collider c)
+	{
+		tesla = c.GetComponent<TeslaShield>();
+		if ((!(tesla != null) || !tesla.PlayerOnOwnersTeam(base.OwnerID)) && explodeOnTriggerEnter)
+		{
+			Explode(c.gameObject);
+		}
+	}
 
-    protected virtual void OnCollisionEnter(Collision c)
-    {
-        if (explodeOnCollision)
-        {
-            Explode(c.gameObject);
-        }
-    }
+	protected virtual void OnCollisionEnter(Collision c)
+	{
+		if (explodeOnCollision)
+		{
+			Explode(c.gameObject);
+		}
+	}
 
-    public virtual void Explode(GameObject objectHit)
-    {
-        if (ignoreOwner)
-        {
-            PlayerController component = objectHit.GetComponent<PlayerController>();
-            if (component != null && component.OwnerID == base.OwnerID)
-            {
-                return;
-            }
-        }
-        if (objectToSpawn != null && !hasSpawned)
-        {
-            CreateExplosion();
-            hasSpawned = true;
-        }
-        DoCameraShake();
-        TryDestroy();
-    }
+	public virtual void Explode(GameObject objectHit)
+	{
+		if (ignoreOwner)
+		{
+			PlayerController component = objectHit.GetComponent<PlayerController>();
+			if (component != null && component.OwnerID == base.OwnerID)
+			{
+				return;
+			}
+		}
+		if (objectToSpawn != null && !hasSpawned)
+		{
+			CreateExplosion();
+			hasSpawned = true;
+		}
+		DoCameraShake();
+		TryDestroy();
+	}
 
-    protected virtual void CreateExplosion()
-    {
-        GameObject gameObject = PhotonNetwork.Instantiate(objectToSpawn.gameObject.name, transform.position, Quaternion.identity, 0);
-        ConfigurableNetworkObject component = gameObject.GetComponent<ConfigurableNetworkObject>();
-        component.OwnerID = base.OwnerID;
-        component.DamageMultiplier = base.DamageMultiplier;
-        component.SetItemOverride(spawnItemOverride);
-        component.SetEquipmentNames(equipmentNames);
-        if (base.GetComponent<Rigidbody>() != null && (_orientX || _orientY || _orientZ))
-        {
-            gameObject.transform.LookAt(base.transform.position + base.GetComponent<Rigidbody>().velocity.normalized);
-            Vector3 eulerAngles = gameObject.transform.rotation.eulerAngles;
-            if (!_orientX)
-            {
-                eulerAngles.x = 0f;
-            }
-            if (!_orientY)
-            {
-                eulerAngles.y = 0f;
-            }
-            if (!_orientZ)
-            {
-                eulerAngles.z = 0f;
-            }
-            gameObject.transform.eulerAngles = eulerAngles;
-        }
-        if (tesla != null && gameObject.GetComponent<Collider>() != null)
-        {
-            Physics.IgnoreCollision(tesla.PlayerController.DamageReceiver.GetComponent<Collider>(), gameObject.GetComponent<Collider>());
-        }
-    }
+	protected virtual void CreateExplosion()
+	{
+		GameObject gameObject = Object.Instantiate(objectToSpawn.gameObject, base.transform.position, Quaternion.identity) as GameObject;
+		ConfigurableNetworkObject component = gameObject.GetComponent<ConfigurableNetworkObject>();
+		component.OwnerID = base.OwnerID;
+		component.DamageMultiplier = base.DamageMultiplier;
+		component.SetItemOverride(spawnItemOverride);
+		component.SetEquipmentNames(equipmentNames);
+		if (base.GetComponent<Rigidbody>() != null && (_orientX || _orientY || _orientZ))
+		{
+			gameObject.transform.LookAt(base.transform.position + base.GetComponent<Rigidbody>().velocity.normalized);
+			Vector3 eulerAngles = gameObject.transform.rotation.eulerAngles;
+			if (!_orientX)
+			{
+				eulerAngles.x = 0f;
+			}
+			if (!_orientY)
+			{
+				eulerAngles.y = 0f;
+			}
+			if (!_orientZ)
+			{
+				eulerAngles.z = 0f;
+			}
+			gameObject.transform.eulerAngles = eulerAngles;
+		}
+		if (tesla != null && gameObject.GetComponent<Collider>() != null)
+		{
+			Physics.IgnoreCollision(tesla.PlayerController.DamageReceiver.GetComponent<Collider>(), gameObject.GetComponent<Collider>());
+		}
+	}
 
-    protected void DoCameraShake()
-    {
-        if (shakeScreen && Camera.main != null)
-        {
-            float f = (explosionShakeRadius - Vector3.Distance(base.transform.position, Camera.main.transform.position)) / explosionShakeRadius;
-            float num = Mathf.Pow(f, shakeFunctionExponent);
-            if (num > 0f)
-            {
-                ShakeCamera shakeCamera = Camera.main.gameObject.AddComponent<ShakeCamera>();
-                shakeCamera.shakeDuration = shakeDuration;
-                shakeCamera.shakeStrength = shakeStrength * num;
-            }
-        }
-    }
+	protected void DoCameraShake()
+	{
+		if (shakeScreen && Camera.main != null)
+		{
+			float f = (explosionShakeRadius - Vector3.Distance(base.transform.position, Camera.main.transform.position)) / explosionShakeRadius;
+			float num = Mathf.Pow(f, shakeFunctionExponent);
+			if (num > 0f)
+			{
+				ShakeCamera shakeCamera = Camera.main.gameObject.AddComponent<ShakeCamera>();
+				shakeCamera.shakeDuration = shakeDuration;
+				shakeCamera.shakeStrength = shakeStrength * num;
+			}
+		}
+	}
 
-    public virtual void TryDestroy()
-    {
-        Object.Destroy(base.gameObject);
-    }
+	public virtual void TryDestroy()
+	{
+		Object.Destroy(base.gameObject);
+	}
 
-    public virtual void OnDisable()
-    {
-        if (_tryToSpawnOnDisable && objectToSpawn != null && !hasSpawned)
-        {
-            CreateExplosion();
-        }
-    }
+	public virtual void OnDisable()
+	{
+		if (_tryToSpawnOnDisable && objectToSpawn != null && !hasSpawned)
+		{
+			CreateExplosion();
+		}
+	}
 }
